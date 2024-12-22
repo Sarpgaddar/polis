3df2cd225329a9321ba27a9a7ce59ec1862df6c3
bashar <22010903026@subu.edu.tr>
2024-12-22 18:03:11 +0300

    لا شيء لعرضه

--- game.cpp
+++ game.cpp
@@ -1,17 +1,21 @@
 #include "game.h"
 
 // بناء اللعبة
+// كيفحال
 Game::Game() : player(0, 0), thief(5, 5) {}
 
 // تشغيل اللعبة
-void Game::start() {
+void Game::start()
+{
     char move;
-    while (true) {
+    while (true)
+    {
         system("clear"); // تنظيف الشاشة
         player.display();
         thief.display();
 
-        if (checkCollision()) {
+        if (checkCollision())
+        {
             cout << "The police caught the thief! Game Over." << endl;
             break;
         }
@@ -26,6 +30,7 @@ void Game::start() {
 }
 
 // التحقق من الاصطدام بين الشرطي والحرامي
-bool Game::checkCollision() {
+bool Game::checkCollision()
+{
     return player.getX() == thief.getX() && player.getY() == thief.getY();
 }
